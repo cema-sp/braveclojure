408ea48bce9d09008f3b9aa22066819f3ddd9108
cema-sp <cema.rus@gmail.com>
2015-10-20 12:31:44 +0300

    Implements additional tasks

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -36,6 +36,30 @@
   [minimum-glitter records]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
 
+(defn names-for
+  [records]
+  (map #(:name %) records))
+
+(defn prepend
+  [name glitter-index list]
+  (cons {:name name :glitter-index glitter-index} list))
+
+(def not-nil? (complement nil?))
+
+(def validations {:name not-nil?
+                  :glitter-index not-nil?})
+
+(defn validate
+  [record]
+  (every? true? (map #((% validations) (% record)) (into [] (keys validations)))))
+
+(defn csvfy
+  "Convert map to CSV"
+  [records]
+  (let [records-w-headers (prepend "Name" "Glitter Index" records)]
+    (s/join "\n"
+            (map #(s/join "," (vals %)) records-w-headers))))
+
 ;;(defn -main
 ;;  "I don't do a whole lot ... yet."
 ;;  [& args]
